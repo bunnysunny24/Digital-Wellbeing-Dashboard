9f629ec5825cc86ba85e7b4751f1b40be9402225
bunnysunny24 <bhavashesh@gmail.com>
2025-04-24 21:13:30 +0530

    txtfffydece

---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    index: './web/index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'web/dist'),
+    publicPath: '/',
+    filename: '[name].bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules\/(?!(react-native-vector-icons)\/).*/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@react-native/babel-preset'],
+            plugins: ['react-native-web'],
+          },
+        },
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'images/[name].[hash:8].[ext]',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.ttf$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'fonts/[name].[hash:8].[ext]',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.web.js', '.js', '.web.jsx', '.jsx', '.web.ts', '.ts', '.web.tsx', '.tsx'],
+    alias: {
+      'react-native$': 'react-native-web',
+      'react-native-vector-icons': 'react-native-vector-icons/dist',
+    },
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      __DEV__: process.env.NODE_ENV !== 'production',
+    }),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'web'),
+    historyApiFallback: true,
+    port: 8080,
+    open: true,
+  },
+};
