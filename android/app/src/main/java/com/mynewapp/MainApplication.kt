31f9d6062a95b7ffc87d57fe14c19083aec2dd9e
bunnysunny24 <bhavashesh@gmail.com>
2025-04-26 14:36:23 +0530

    wcre

--- android/app/src/main/java/com/mynewapp/MainApplication.kt
+++ android/app/src/main/java/com/mynewapp/MainApplication.kt
@@ -1,6 +1,7 @@
 package com.mynewapp
 
 import android.app.Application
+import android.util.Log
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
@@ -35,10 +36,14 @@ class MainApplication : Application(), ReactApplication {
 
   override fun onCreate() {
     super.onCreate()
-    SoLoader.init(this, OpenSourceMergedSoMapping)
-    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      // If you opted-in for the New Architecture, we load the native entry point for this app.
-      load()
+    try {
+      SoLoader.init(this, false)
+      if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+        load()
+      }
+      OpenSourceMergedSoMapping.loadMergedSoMapping()
+    } catch (e: Exception) {
+      Log.e("MainApplication", "Failed to initialize: ${e.message}", e)
     }
   }
 }
